e084d986dda60228d2e7dc1ca6cace3fa1f237de
raihanwebmaster <raihanemon2015@gmail.com>
2021-02-04 18:48:22 +0100

    client

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+import Navbar from './components/Navbar/Navbar';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Home from './components/Home/Home';
+import Auth from './components/Auth/Auth';
+
+const App = () => (
+  <BrowserRouter>
+    <Container maxWidth="lg">
+      <Navbar/>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/auth" exact component={Auth} />
+      </Switch>
+
+       
+    </Container>
+  </BrowserRouter>
+);
+
+export default App;
